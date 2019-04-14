6ed98a3981a20e7ac8e26463743c87390f822cbe
caseydulong <caseydulong@gmail.com>
2019-04-14 16:13:03 -0400

    Adds update, destroy, and show curl scripts for encounters

--- curl-scripts/encounters/update.sh
+++ curl-scripts/encounters/update.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/encounters"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
---header "Authorization: Bearer ${TOKEN}" \
---data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "encounter": {
+      "owner": "'"${OWNER}"'",
+      "combatants": [
+        {"name": "Ignatius", "initiative": 20},
+        {"name": "Reliquary", "initiative": 15},
+        {"name": "Izadora", "initiative": 10}
+      ]
     }
   }'
 
